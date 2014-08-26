94e312b2ffea6a36019c0299945978eae9a66136
peferron <pe.ferron@gmail.com>
2014-08-26 16:48:31 +0800

    Add unit tests for filter Ufl

---
+++ test/unit/Ufl.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('filter Ufl (UppercaseFirstLetter)', function() {
+    beforeEach(module('lmFilters'));
+
+    var ufl;
+    beforeEach(inject(function(UflFilter) {
+        ufl = UflFilter;
+    }));
+
+    it('should make the first letter uppercase', function() {
+        ufl('').should.equal('');
+        ufl('a').should.equal('A');
+        ufl('A').should.equal('A');
+        ufl('abc').should.equal('Abc');
+        ufl('abC').should.equal('AbC');
+        ufl('1').should.equal('1');
+        ufl('爱').should.equal('爱');
+    });
+});
