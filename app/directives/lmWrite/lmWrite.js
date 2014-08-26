835ccf37a28eb7c2ad4daa4e5737c64b48ed1f74
peferron <pe.ferron@gmail.com>
2014-08-26 15:53:55 +0800

    Remove mailchimp latest

--- app/directives/lmWrite/lmWrite.js
+++ app/directives/lmWrite/lmWrite.js
@@ -1,25 +1,25 @@
-'use strict';
+// 'use strict';
 
-angular
-    .module('lmDirectives')
-    .directive('lmWrite', function() {
-        return {
-            restrict: 'E',
-            link: function(scope, element, attrs) {
-                /* jshint evil: true */
+// angular
+//     .module('lmDirectives')
+//     .directive('lmWrite', function() {
+//         return {
+//             restrict: 'E',
+//             link: function(scope, element, attrs) {
+//                 /* jshint evil: true */
 
-                var oldWrite = document.write;
-                document.write = function (str) {
-                    element.html(str);
-                };
+//                 var oldWrite = document.write;
+//                 document.write = function (str) {
+//                     element.html(str);
+//                 };
 
-                var s = document.createElement('script');
-                s.src = attrs.src;
-                s.onload = function() {
-                    document.write = oldWrite;
-                    element.addClass('loaded');
-                };
-                element.append(s);
-            }
-        };
-    });
+//                 var s = document.createElement('script');
+//                 s.src = attrs.src;
+//                 s.onload = function() {
+//                     document.write = oldWrite;
+//                     element.addClass('loaded');
+//                 };
+//                 element.append(s);
+//             }
+//         };
+//     });
