c36f6d5aa41c4d5970525952651734b1e8778c08
peferron <pe.ferron@gmail.com>
2014-09-29 13:11:10 +0800

    Move generic article-loading code in posts.js to the dedicated Articles service

---
+++ app/services/Articles.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular
+    .module('lmServices')
+    .service('Articles', function(Article, Admin) {
+        this.load = function(defaultParams, $scope, callback) {
+            $scope.loadStatus = 'loading';
+
+            $scope.articles = $scope.articles || [];
+            var params = defaultParams ? angular.copy(defaultParams) : {};
+
+            var length = $scope.articles.length;
+            if (!length) {
+                // First load.
+                params.validatedBefore = '0';
+                params.validatedOnly = !Admin;
+            } else {
+                // Subsequent loads.
+                params.validatedBefore = $scope.articles[length - 1].firstValidationTimeNano;
+                // Only the first load should get non-validated articles, otherwise they will
+                // end up duplicated.
+                params.validatedOnly = true;
+            }
+
+            Article.query(params, function(articles) {
+                // This is efficient because Angular will not recreate DOM elements for the
+                // already-appended articles.
+                $scope.articles = $scope.articles.concat(articles);
+
+                if (articles.length < params.count ||
+                    articles.length && !articles[articles.length - 1].validated) {
+                    $scope.loadStatus = 'ended';
+                } else {
+                    $scope.loadStatus = 'ready';
+                }
+
+                if (callback) {
+                    callback();
+                }
+            });
+        };
+    });
--- app/views/posts/posts.js
+++ app/views/posts/posts.js
@@ -3,48 +3,16 @@
 angular
     .module('lmControllers')
     .controller('posts',
-        function($rootScope, $scope, Article, Analytics, Admin) {
+        function($rootScope, $scope, Articles, Analytics) {
             $rootScope.title = 'Posts — LilyMandarin';
             Analytics.page();
 
             $rootScope.tab = 'posts';
 
-            $scope.posts = [];
+            var params = {categories: 'post', count: 20};
 
-            // Fetches and appends a new batch of posts
             $scope.load = function() {
-                $scope.loadStatus = 'loading';
-
-                var params = {
-                    categories: 'post',
-                    count: 20
-                };
-
-                var length = $scope.posts.length;
-                if (!length) {
-                    // First load
-                    params.validatedBefore = '0';
-                    params.validatedOnly = !Admin;
-                } else {
-                    // Subsequent loads
-                    params.validatedBefore = $scope.posts[length - 1].firstValidationTimeNano;
-                    // Only the first load should get non-validated articles, otherwise they will
-                    // end up duplicated.
-                    params.validatedOnly = true;
-                }
-
-                Article.query(params, function(posts) {
-                    // This is efficient because Angular will not recreate DOM elements for the
-                    // already-appended posts in $scope.posts
-                    $scope.posts = $scope.posts.concat(posts);
-
-                    if (posts.length < params.count ||
-                        posts.length && !posts[posts.length - 1].validated) {
-                        $scope.loadStatus = 'ended';
-                    } else {
-                        $scope.loadStatus = 'ready';
-                    }
-                });
+                Articles.load(params, $scope);
             };
 
             $scope.load();
