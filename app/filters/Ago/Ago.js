b036b6e698f783c89e0be2d8a50fc271c7646f57
peferron <pe.ferron@gmail.com>
2014-10-01 15:59:05 +0800

    Comment out Ago and FancyDate filters (not used anymore)

--- app/filters/Ago/Ago.js
+++ app/filters/Ago/Ago.js
@@ -1,46 +1,46 @@
-'use strict';
+// 'use strict';
 
-angular
-    .module('lmFilters')
-    .filter('Ago', function($filter) {
-        // Converts a time ago expressed in milliseconds to a pretty sentence.
-        return function(ago) {
-            if (typeof ago !== 'number' || isNaN(ago)) {
-                return ago;
-            }
+// angular
+//     .module('lmFilters')
+//     .filter('Ago', function($filter) {
+//         // Converts a time ago expressed in milliseconds to a pretty sentence.
+//         return function(ago) {
+//             if (typeof ago !== 'number' || isNaN(ago)) {
+//                 return ago;
+//             }
 
-            var s = Math.round(ago / 1000);
-            if (s < 10) {
-                return 'just now';
-            }
-            var prefix = ' ago';
-            var f = Math.floor;
-            var p = $filter('Plural');
-            if (s < 60) {
-                return p(f(s / 10) * 10, 'second') + prefix;
-            }
-            if (s < 3600) {
-                return p(f(s / 60), 'minute') + prefix;
-            }
-            if (s < 3600 * 24) {
-                return p(f(s / 3600), 'hour') + prefix;
-            }
-            if (s < 3600 * 24 * 31) {
-                return p(f(s / 3600 / 24), 'day') + prefix;
-            }
-            var daysPerMonth = 365.25 / 12;
-            var months = function(value) {
-                return p(f(value / 3600 / 24 / daysPerMonth), 'month') + prefix;
-            };
-            if (s < 3600 * 24 * daysPerMonth * 12) {
-                return months(s);
-            }
-            var y = f(s / 3600 / 24 / daysPerMonth / 12);
-            var r = s - y * 3600 * 24 * daysPerMonth * 12;
-            y = p(y, 'year');
-            if (r < 3600 * 24 * daysPerMonth) {
-                return y + prefix;
-            }
-            return y + ' and ' + months(r);
-        };
-    });
+//             var s = Math.round(ago / 1000);
+//             if (s < 10) {
+//                 return 'just now';
+//             }
+//             var prefix = ' ago';
+//             var f = Math.floor;
+//             var p = $filter('Plural');
+//             if (s < 60) {
+//                 return p(f(s / 10) * 10, 'second') + prefix;
+//             }
+//             if (s < 3600) {
+//                 return p(f(s / 60), 'minute') + prefix;
+//             }
+//             if (s < 3600 * 24) {
+//                 return p(f(s / 3600), 'hour') + prefix;
+//             }
+//             if (s < 3600 * 24 * 31) {
+//                 return p(f(s / 3600 / 24), 'day') + prefix;
+//             }
+//             var daysPerMonth = 365.25 / 12;
+//             var months = function(value) {
+//                 return p(f(value / 3600 / 24 / daysPerMonth), 'month') + prefix;
+//             };
+//             if (s < 3600 * 24 * daysPerMonth * 12) {
+//                 return months(s);
+//             }
+//             var y = f(s / 3600 / 24 / daysPerMonth / 12);
+//             var r = s - y * 3600 * 24 * daysPerMonth * 12;
+//             y = p(y, 'year');
+//             if (r < 3600 * 24 * daysPerMonth) {
+//                 return y + prefix;
+//             }
+//             return y + ' and ' + months(r);
+//         };
+//     });
--- app/filters/FancyDate/FancyDate.js
+++ app/filters/FancyDate/FancyDate.js
@@ -1,39 +1,39 @@
-'use strict';
+// 'use strict';
 
-angular
-    .module('lmFilters')
-    .filter('FancyDate', function($filter) {
-        // Returns the ordinal suffix ('st', 'nd' or 'th') for the day of the given date.
-        function getSuffix(date) {
-            var d = date.getDate();
-            switch (d) {
-                case 1:
-                case 21:
-                case 31:
-                    return 'st';
-                case 2:
-                case 22:
-                    return 'nd';
-                case 3:
-                case 23:
-                    return 'rd';
-                default:
-                    return 'th';
-            }
-        }
+// angular
+//     .module('lmFilters')
+//     .filter('FancyDate', function($filter) {
+//         // Returns the ordinal suffix ('st', 'nd' or 'th') for the day of the given date.
+//         function getSuffix(date) {
+//             var d = date.getDate();
+//             switch (d) {
+//                 case 1:
+//                 case 21:
+//                 case 31:
+//                     return 'st';
+//                 case 2:
+//                 case 22:
+//                     return 'nd';
+//                 case 3:
+//                 case 23:
+//                     return 'rd';
+//                 default:
+//                     return 'th';
+//             }
+//         }
 
-        // Converts a time expressed in milliseconds to a pretty date.
-        return function(time) {
-            if (typeof time !== 'number' || isNaN(time)) {
-                return time;
-            }
+//         // Converts a time expressed in milliseconds to a pretty date.
+//         return function(time) {
+//             if (typeof time !== 'number' || isNaN(time)) {
+//                 return time;
+//             }
 
-            var date = new Date(time);
-            var ngDate = $filter('date');
+//             var date = new Date(time);
+//             var ngDate = $filter('date');
 
-            var str = ngDate(date, 'MMMM d, y');
-            var suffix = getSuffix(date);
+//             var str = ngDate(date, 'MMMM d, y');
+//             var suffix = getSuffix(date);
 
-            return str.replace(',', suffix + ',');
-        };
-    });
+//             return str.replace(',', suffix + ',');
+//         };
+//     });
