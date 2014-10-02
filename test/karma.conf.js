32f51c1127175977e7a34da62477f203126568f6
peferron <pe.ferron@gmail.com>
2014-10-02 14:06:04 +0800

    Add full jquery support and html2js preprocessing to tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
     config.set({
         basePath : '../',
         files : [
+            'node_modules/jquery/dist/jquery.js',
             'node_modules/chai-angular/chai-angular.js',
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
@@ -13,14 +14,27 @@ module.exports = function(config) {
             'app/app.js',
             'app/route.js',
             'app/directives/**/*.js',
+            'app/directives/**/*.html',
             'app/filters/**/*.js',
             'app/services/**/*.js',
             'app/views/**/*.js'
         ],
         autoWatch : true,
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['chai', 'mocha'],
         reporters: ['progress'],
         browsers : ['Chrome'],
-        plugins : ['karma-chrome-launcher', 'karma-mocha', 'karma-chai']
+        plugins : [
+            'karma-chai',
+            'karma-chrome-launcher',
+            'karma-mocha',
+            'karma-ng-html2js-preprocessor'
+        ],
+        preprocessors: {
+            'app/directives/**/*.html': ['ng-html2js']
+        },
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'app',
+            moduleName: 'lmTemplates'
+        }
     });
 };
