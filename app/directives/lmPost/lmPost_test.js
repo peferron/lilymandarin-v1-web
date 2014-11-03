b939c3a9d494565a9a15cc8bb6bd19aaffd8886c
peferron <pe.ferron@gmail.com>
2014-11-02 16:12:03 -0800

    Add test for directive lmPost

---
+++ app/directives/lmPost/lmPost_test.js
@@ -0,0 +1,59 @@
+/* global describe: false, beforeEach: false, module: false, inject: false, it: false */
+
+'use strict';
+
+describe('directive lmPost', function() {
+    var element, $compile, $rootScope;
+
+    beforeEach(module('lmDirectives', 'lmTemplates', 'lmFilters', 'lmServices'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    beforeEach(inject(function($compile, $rootScope) {
+        $rootScope.post = {
+            id: 'abcid',
+            type: 'photo',
+            title: 'abctitle',
+            slug: 'abcslug',
+            internal: {
+                medias: {
+                    'main-photo': {
+                        height: 345,
+                        width: 678,
+                        filename: 'abcfilename'
+                    }
+                }
+            },
+            lines: [
+                {
+                    'zh-CN': 'abczhcn',
+                    'pinyin': 'abcpinyin',
+                    'en-US': 'abcenus'
+                }
+            ]
+        };
+        element = $compile('<lm-post post="post"></lm-post>')($rootScope);
+        $rootScope.$digest();
+    }));
+
+    it('sets the img src', function() {
+        var img = element.find('img');
+        img.should.have.length(1);
+        img.attr('src').should.equal('/media/abcfilename_w400p.jpg');
+    });
+
+    it('sets the lines', function() {
+        element.find('.zh-cn').text().should.equal('abczhcn');
+        element.find('.pinyin').text().should.equal('abcpinyin');
+        element.find('.en-us').text().should.equal('abcenus');
+    });
+
+    it('sets the social url and text', function() {
+        var $scope = element.isolateScope();
+        $scope.socialUrl.should.equal('http://lilymandarin.com/post/abcid/abcslug');
+        $scope.socialText.should.equal('abczhcn / abcenus');
+    });
+});
