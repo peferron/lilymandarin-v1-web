9c7dd645ec0537bd2c939a1943f29d62b2f50278
peferron <pe.ferron@gmail.com>
2014-11-10 09:45:13 -0800

    Add test cases for directive lmTopHalf

--- app/directives/lmTopHalf/lmTopHalf_test.js
+++ app/directives/lmTopHalf/lmTopHalf_test.js
@@ -3,23 +3,28 @@
 'use strict';
 
 describe('directive lmTopHalf', function() {
-    var element;
+    var element, $rootScope;
 
-    beforeEach(module('lmDirectives', 'lmTemplates'));
+    var mockWindow = {
+        innerHeight: NaN,
+        document: {
+            body: {
+                clientWidth: NaN
+            }
+        }
+    };
 
-    function compile(width, height, ratio) {
-        module(function($provide) {
-            $provide.value('$window', {
-                innerHeight: height,
-                document: {
-                    body: {
-                        clientWidth: width
-                    }
-                }
-            });
-        });
-        inject(function($compile, $rootScope) {
-            element = $compile('<lm-top-half ratio="' + ratio + '"><span class="content"></span></lm-top-half>')($rootScope);
+    beforeEach(module('lmDirectives', 'lmTemplates', function($provide) {
+        $provide.value('$window', mockWindow);
+    }));
+
+    beforeEach(inject(function(_$rootScope_) {
+        $rootScope = _$rootScope_;
+    }));
+
+    function compile() {
+        inject(function($compile) {
+            element = $compile('<lm-top-half ratio="ratio"><span class="content"></span></lm-top-half>')($rootScope);
             $rootScope.$digest();
         });
     }
@@ -31,15 +36,49 @@ describe('directive lmTopHalf', function() {
 
     describe('with available width = 100px, available height = 100px, and ratio = 1.5', function() {
         beforeEach(function() {
-            compile(100, 100, 1.5);
+            mockWindow.document.body.clientWidth = 100;
+            mockWindow.innerHeight = 100;
+            $rootScope.ratio = 1.5;
+            compile();
         });
 
-        it('sets the element height to 50px and the content width to 75px', function() {
+        it('sets the element height to 50px', function() {
             element.css('height').should.equal('50px');
         });
 
         it('sets the content width to 75px', function() {
             element.find('.lm-top-half__content').css('width').should.equal('75px');
+        });
+
+        describe('then ratio changed to 2', function() {
+            beforeEach(function() {
+                $rootScope.ratio = 2;
+                element.isolateScope().$digest();
+            });
+
+            it('sets the element height to 50px', function() {
+                element.css('height').should.equal('50px');
+            });
+
+            it('sets the content width to 100px', function() {
+                element.find('.lm-top-half__content').css('width').should.equal('100px');
+            });
+        });
+
+        describe('then available width changed to 200px and available height changed to 150px', function() {
+            beforeEach(function() {
+                mockWindow.document.body.clientWidth = 200;
+                mockWindow.innerHeight = 150;
+                angular.element(mockWindow).triggerHandler('resize');
+            });
+
+            it('sets the element height to 75px', function() {
+                element.css('height').should.equal('75px');
+            });
+
+            it('sets the content width to 112.5px', function() {
+                element.find('.lm-top-half__content').css('width').should.equal('112.5px');
+            });
         });
     });
 });
