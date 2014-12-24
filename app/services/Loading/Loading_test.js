787f0ed8e5b78996aba24c9e6d4f8f28fc6c14d3
peferron <pe.ferron@gmail.com>
2014-12-23 21:39:29 -0800

    Add test for service Loading

---
+++ app/services/Loading/Loading_test.js
@@ -0,0 +1,41 @@
+/* global describe: false, beforeEach: false, module: false, inject: false, it: false */
+
+'use strict';
+
+describe('service Loading', function() {
+    var $rootScope, Loading;
+
+    beforeEach(module('lmServices'));
+
+    beforeEach(inject(function(_$rootScope_, _Loading_) {
+        $rootScope = _$rootScope_;
+        Loading = _Loading_;
+    }));
+
+    it('should init $rootScope.loading to false', function() {
+        $rootScope.loading.should.equal(false);
+    });
+
+    it('should set $rootScope.loading to true after a request is received', function() {
+        Loading.request();
+        $rootScope.loading.should.equal(true);
+    });
+
+    it('should set $rootScope.loading to false after all requests are answered', function() {
+        Loading.request();
+        Loading.request();
+        Loading.response();
+        Loading.responseError();
+        $rootScope.loading.should.equal(false);
+    });
+
+    it('should keep $rootScope.loading to true if some requests are still pending', function() {
+        Loading.request();
+        Loading.request();
+        Loading.response();
+        Loading.request();
+        Loading.responseError();
+        $rootScope.loading.should.equal(true);
+    });
+
+});
