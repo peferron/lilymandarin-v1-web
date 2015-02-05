b57fe923387493ae9c25fb657787116a1ff13c44
peferron <pe.ferron@gmail.com>
2015-02-05 00:08:35 -0800

    Update lmCarousel for compatibility with Angular 1.3.12

--- app/directives/lmCarousel/lmCarousel.js
+++ app/directives/lmCarousel/lmCarousel.js
@@ -8,8 +8,11 @@ angular
             controller: function($scope, $attrs, $timeout) {
                 $scope.carousel = 0;
 
-                var period = 0;
-                var max = 0;
+                var DEFAULT_PERIOD = 4;
+                var DEFAULT_MAX = Number.MAX_VALUE;
+
+                var period = DEFAULT_PERIOD;
+                var max = DEFAULT_MAX;
                 var timeout;
 
                 function run() {
@@ -18,28 +21,50 @@ angular
                         $scope.carousel = Math.max(0, max - 1);
                     }
 
-                    // Don't start a timeout if it's already started or if the input parameters are
-                    // invalid.
-                    if (timeout || period <= 0 || max < 2) {
+                    // Don't start a timeout if it's already started.
+                    if (timeout) {
                         return;
                     }
 
-                    timeout = $timeout(function() {
-                        $scope.carousel = ($scope.carousel + 1) % max;
-                        timeout = null;
-                        run();
-                    }, period * 1000);
+                    timeout = $timeout(onTimeout, period * 1000);
+                }
+
+                function onTimeout() {
+                    $scope.carousel = ($scope.carousel + 1) % max;
+                    timeout = null;
+                    run();
+                }
+
+                function onPeriodChange(value) {
+                    if (angular.isDefined(value)) {
+                        var num = parseFloat(value);
+                        if (isNaN(num) || num <= 0) {
+                            throw new Error('lmCarousel: period must be a number > 0');
+                        }
+                        period = num;
+                    } else {
+                        period = DEFAULT_PERIOD;
+                    }
+                    run();
+                }
+
+                function onMaxChange(value) {
+                    if (angular.isDefined(value)) {
+                        var num = parseInt(value, 10);
+                        if (isNaN(num) || num < 2) {
+                            throw new Error('lmCarousel: max must be an integer >= 2');
+                        }
+                        max = num;
+                    } else {
+                        max = DEFAULT_MAX;
+                    }
+                    run();
                 }
 
                 // Follow changes to input parameters.
-                $attrs.$observe('period', function(value) {
-                    period = angular.isDefined(value) ? value : 4;
-                    run();
-                });
-                $attrs.$observe('max', function(value) {
-                    max = angular.isDefined(value) ? value : Number.MAX_VALUE;
-                    run();
-                });
+                $attrs.$observe('period', onPeriodChange);
+                $attrs.$observe('max', onMaxChange);
+                run();
             }
         };
     });
