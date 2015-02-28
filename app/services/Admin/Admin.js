109b89e89845eaa7371d74ab603ba993be0fb504
peferron <pe.ferron@gmail.com>
2015-02-28 14:59:52 -0800

    Add comments explaining how the admin interface works

--- app/services/Admin/Admin.js
+++ app/services/Admin/Admin.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// This "admin" cookie is not intended to be secure. All it does is display a few convenience
+// buttons for quick access to the admin interface. Access to the admin interface is unrestricted;
+// the admin interface merely displays the same data as the normal web interface, but in an
+// editable manner. Authentication happens server-side when the user attempts to save changes.
 angular
     .module('lmServices')
     .constant('Admin', (/((; )|^)admin=true(;|$)/).test(document.cookie));
