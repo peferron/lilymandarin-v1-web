6b2aef33b885cf832c0184532a3d90f9efc91d05
peferron <pe.ferron@gmail.com>
2015-04-18 15:50:53 -0700

    Fix review modal close buttons

--- app/views/reviews/reviews.js
+++ app/views/reviews/reviews.js
@@ -2,11 +2,13 @@
 
 angular
     .module('lmControllers')
-    .controller('reviews', function($rootScope, $scope, Analytics, Review) {
+    .controller('reviews', function($rootScope, $scope, Analytics, Review, Modal) {
         if (!$rootScope.modalTemplateUrl) {
             $rootScope.title = 'Student reviews — LilyMandarin';
             $rootScope.tab = 'home';
             Analytics.page();
+        } else {
+            $scope.hideModal = Modal.hide;
         }
 
         $scope.reviews = Review.query();
